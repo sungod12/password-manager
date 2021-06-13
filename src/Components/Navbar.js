7fd403ab47e5b3df27fee6f5466991de54493e90
Suraj Prabhu <prabhusuraj103@gmail.com>
2021-06-13 12:12:31 +0530

    refactored css for navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,59 +5,59 @@ function Navbar({ uname, logout }) {
   const [menuOpen, setMenuOpen] = React.useState(false);
   const Component = () => {
     return (
-      <div className="flex flex-wrap py-2 md:px-3.5 ">
-        <div className="w-full">
-          <nav className="relative flex flex-wrap items-center justify-between py-3 rounded">
-            <div className="container  mx-auto flex flex-wrap items-center justify-between">
-              <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start ">
+      <div className="navigation-container">
+        <div className="w-full py-2.5">
+          <nav className="nav-container ">
+            <div className="nav-div">
+              <div className="navigation">
                 <img
-                  className="inline-block w-8 xl:w-10"
+                  className="nav-logo"
                   src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                   alt="passaver-logo"
                 />
-                <button
-                  className="text-white cursor-pointer text-xl leading-none  py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                  type="button"
-                  onClick={() => setMenuOpen(!menuOpen)}
-                >
-                  <i className="fas fa-bars"></i>
-                </button>
+                <div className="flex items-center">
+                  <p className="username">{uname}</p>
+                  <button
+                    className="menu-button"
+                    type="button"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                  >
+                    {uname ? (
+                      <p className="user-icon">{uname.substr(0, 1)}</p>
+                    ) : (
+                      <i className="fas fa-bars"></i>
+                    )}
+                  </button>
+                </div>
               </div>
               <div
                 className={
-                  "lg:flex flex-grow items-center " +
+                  "sm:flex flex-grow items-center " +
                   (menuOpen ? " flex" : " hidden")
                 }
                 id="example-navbar-info"
               >
-                <ul className="px-4 list-none ml-auto ">
-                  <li className="nav-item flex flex-col items-end w-28 pr-2.5 bg-blue-400 rounded-lg lg:flex-row lg:space-x-2 lg:rounded-none lg:bg-transparent lg:w-auto">
-                    {/* <NavLink
-                      className="py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                      to="/"
-                    >
-                      Profile
-                    </NavLink> */}
+                <ul className="list">
+                  <li className="nav-item navlink-list">
                     {uname ? (
-                      <button
-                        type="button"
-                        className=" py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                        onClick={logout}
-                      >
-                        Logout
-                      </button>
+                      <>
+                        {/* <NavLink className="profile-link" to="/">
+                          Profile
+                        </NavLink> */}
+                        <button
+                          type="button"
+                          className=" logout-button"
+                          onClick={logout}
+                        >
+                          Logout
+                        </button>
+                      </>
                     ) : (
                       <>
-                        <NavLink
-                          className="py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                          to="/"
-                        >
+                        <NavLink className="register-link" to="/">
                           Register
                         </NavLink>
-                        <NavLink
-                          className=" py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                          to="/signIn"
-                        >
+                        <NavLink className=" signin-link" to="/signIn">
                           Sign In
                         </NavLink>
                       </>

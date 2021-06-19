ce1f12cdb1c86262643916a49293f88c74a6ee13
Suraj Prabhu <prabhusuraj103@gmail.com>
2021-06-19 20:53:54 +0530

    added alert box instead of modal box

--- src/Components/PasswordReset.js
+++ src/Components/PasswordReset.js
@@ -7,7 +7,7 @@ import Alert from "./Alert";
 function PasswordReset({ history }) {
   const [email, setEmail] = React.useState("");
   const [isOpen, setIsOpen] = React.useState(false);
-  const [response, setResponse] = React.useState("");
+  const [response, setResponse] = React.useState(false);
   const [message, setMessage] = React.useState("");
 
   const handleChange = (e) => {
@@ -18,33 +18,43 @@ function PasswordReset({ history }) {
   const resetPassword = async (e) => {
     e.preventDefault();
     try {
-      setEmail("");
-      setResponse("Success");
-      setMessage("Password ResetMail sent.Please check your inbox.");
-      setIsOpen(true);
       await firebase.auth().sendPasswordResetEmail(email);
-      history.replace("/signIn");
-    } catch (error) {
-      setResponse("Error");
-      setMessage("Email does not exist.");
+      setEmail("");
       setIsOpen(true);
+      setResponse(true);
+      setMessage("Password Reset mail sent.");
+      const timer = () => {
+        setTimeout(() => {
+          history.replace("/signIn");
+        }, 5000);
+      };
+      timer();
+      clearTimeout(timer);
+    } catch (error) {
+      setIsOpen(true);
+      setResponse(false);
+      setMessage("Email does not exist.");
     }
   };
 
   return (
     <>
       <Navbar />
-      <Alert
-        response={response}
-        message={message}
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-      />
+
       <div className="hero-container">
         <div className="reset-container ">
           <h1 className="text-center text-white font-semibold text-xl">
             Forgot Password?
           </h1>
+          {isOpen ? (
+            <Alert
+              message={message}
+              setIsOpen={setIsOpen}
+              response={response}
+            />
+          ) : (
+            ""
+          )}
           <form onSubmit={resetPassword} className="mt-8 space-y-6">
             <div className="space-y-1">
               <p className="font-semibold text-white">Enter your email</p>
